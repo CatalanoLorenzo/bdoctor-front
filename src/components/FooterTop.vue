24a1ba5705698d56848f83a0516eaaeb309c603a
MarioBlasi <mblasi509@gmail.com>
2023-07-07 09:58:07 +0100

    style footer

---
+++ src/components/FooterTop.vue
@@ -0,0 +1,96 @@
+<script>
+export default {
+  name: "FooterTop"
+};
+</script>
+
+<template>
+  <div class="test">
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <i><h4>Progetto Finale</h4></i>
+            <p>&copy;2023 Boolean</p>
+            <b>BDoctor project</b>
+            <p class="m-0 lh-3">Group 4</p>
+          </div>
+          <div class="col milestone">
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+          </div>
+          <div class="col milestone">
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+            <h5>1 h5ermettere ai medici di registrarsi alla h5iattaforma</h5>
+          </div>
+          <div class="col-5">
+            <ul class="menu">
+              <li class="menu__item p-1">
+                <a
+                  class="menu__link"
+                  href="https://github.com/Francesco-Iannuzzi"
+                >
+                  <img
+                    src="https://www.dottorelondon.com/app/uploads/2015/12/doctor1.png"
+                    alt="Francesco Iannuzzi"
+                  />
+                  <p>Francesco</p>
+                </a>
+              </li>
+              <li class="menu__item p-1">
+                <a class="menu__link" href="https://github.com/MarioBlasi">
+                  <img
+                    src="https://www.dottorelondon.com/app/uploads/2015/12/doctor1.png"
+                    alt="Mario Blasi"
+                  />
+                  <p>Mario</p>
+                </a>
+              </li>
+              <li class="menu__item p-1">
+                <a class="menu__link" href="https://github.com/CatalanoLorenzo">
+                  <img
+                    src="https://www.dottorelondon.com/app/uploads/2017/05/laura-ferrara-dottore-london.jpg"
+                    alt="Lorenzo Catalano"
+                  />
+                  <p>Lorenzo</p>
+                </a>
+              </li>
+              <li class="menu__item p-1">
+                <a
+                  class="menu__link"
+                  href="https://github.com/giuseppevignanello"
+                >
+                  <img
+                    src="https://assets.setmore.com/website/v2/images/industry-pages/doctors/online-doctor-appointment.png"
+                    alt="Giuseppe Vignanello"
+                  />
+                  <p>Giuseppe</p>
+                </a>
+              </li>
+              <li class="menu__item p-1">
+                <a
+                  class="menu__link"
+                  href="https://github.com/AlexandroFioretti1"
+                >
+                  <img
+                    src="https://www.dottorelondon.com/app/uploads/2017/05/laura-ferrara-dottore-london.jpg"
+                    alt="Alexandro Fioretti"
+                  />
+                  <p>Alexandro</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
