585800a26305ee2a6a6f89d7529851744c65e612
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-07-11 16:52:06 +0200

    fix validation review

--- src/components/ReviewDoctor.vue
+++ src/components/ReviewDoctor.vue
@@ -17,51 +17,47 @@ export default {
     };
   },
   methods: {
-    checkSubmit() {
-      this.checkError()
-    },
-    checkError() {
-      this.checkErrorName()
-      this.checkErrorSurname()
-      this.checkErrorEmail()
-      this.checkErrorText()
+    checkError(event) {
 
-    },
-    checkErrorName() {
-      if (this.inputName.length > 50) {
+      if (this.inputName.length > 50 || this.inputName.length <= 0) {
         this.errorName = true
+        event.preventDefault()
       } else {
         this.errorName = false
       }
-    },
-    checkErrorSurname() {
-      if (this.inputSurname.length > 50) {
+
+      if (this.inputSurname.length > 50 || this.inputSurname.length <= 0) {
         this.errorSurname = true
+        event.preventDefault()
+
       } else {
         this.errorSurname = false
       }
-    },
-    checkErrorEmail() {
-      if (this.inputEmail.length > 50) {
+
+      if (this.inputEmail.length > 50 || this.inputEmail.length <= 0) {
         this.errorEmail = true
+        event.preventDefault()
+
       } else {
         this.errorEmail = false
       }
-    },
-    checkErrorText() {
-      if (this.inputText.length > 255) {
+
+      if (this.inputText.length > 255 || this.inputText.length <= 0) {
         this.errorText = true
+        event.preventDefault()
+
       } else {
         this.errorText = false
       }
-    },
+
+    }
   }
 }
 </script>
 <template>
   <div>
     <h3>Add a Review</h3>
-    <form :action="store.api_url + 'api/reviews/'" method="post" @submit.prevent="checkSubmit">
+    <form :action="store.api_url + 'api/reviews/'" method="post" @submit="checkError">
       <input type="hidden" :value="store.singleProfile.id" name="profile_id" />
       <input type="hidden" :value="'2023-07-07 15:30:00'" name="date" />
       <input type="hidden" :value="store.singleProfile.slug" name="slug" />
@@ -75,43 +71,43 @@ export default {
       <div class="mb-3">
         <label for="name" class="form-label">Name</label>
         <input v-model="inputName" type="text" class="form-control" name="name" id="name" aria-describedby="helpId"
-          placeholder="type your name and surname">
+          placeholder="Type your name and here">
       </div>
 
       <!-- surname -->
       <div v-show="errorSurname" class="alert alert-danger" role="alert">
-        <strong>Error: </strong> Name error
+        <strong>Error: </strong> The Name must be less then 50 characters
       </div>
 
       <div class="mb-3">
         <label for="surname" class="form-label">Surname</label>
         <input v-model="inputSurname" type="text" class="form-control" name="surname" id="surname"
-          aria-describedby="helpId" placeholder="">
+          aria-describedby="helpId" placeholder="Type your Surname here">
       </div>
 
       <!-- email -->
       <div v-show="errorEmail" class="alert alert-danger" role="alert">
-        <strong>Error: </strong> Name error
+        <strong>Error: </strong> The email isn't correct
       </div>
 
       <div class="mb-3">
         <label for="email" class="form-label">email</label>
         <input v-model="inputEmail" type="text" class="form-control" name="email" id="email" aria-describedby="helpId"
-          placeholder="">
+          placeholder="Type your Email here">
       </div>
 
       <!-- text -->
       <div v-show="errorText" class="alert alert-danger" role="alert">
-        <strong>Error: </strong> Name error
+        <strong>Error: </strong> The Text must be less then 255 characters
       </div>
 
       <div class="mb-3">
-        <label for="text" class="form-label"></label>
+        <label for="text" class="form-label">Reviews</label>
         <textarea v-model="inputText" class="form-control" name="text" id="text" rows="3"></textarea>
       </div>
 
       <!-- submit -->
-      <button @click="checkError()" @submit.prevent='submit' type="submit" class="btn btn-primary">write areview</button>
+      <button @click="checkError()" type="submit" class="btn btn-primary">write a Review</button>
 
     </form>
   </div>
