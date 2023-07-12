882608c7b9d71ff6946c38349542704906fac680
MarioBlasi <mblasi509@gmail.com>
2023-07-12 19:19:33 +0100

    style advanced search

--- src/views/SingleProfile.vue
+++ src/views/SingleProfile.vue
@@ -55,64 +55,103 @@ export default {
       <div class="col-12 bg-light-gray mt-5">
         <div class="d-flex flex-wrap shadow-lg rounded-4 p-4">
           <div class="col-12 col-lg-6 mb-4 mb-lg-0">
-            <img class="rounded-3" :src="store.url_image + store.singleProfile.doctor_image"
-              alt="{{ store.singleProfile.name }}" />
+            <img
+              class="rounded-3"
+              :src="store.url_image + store.singleProfile.doctor_image"
+              alt="{{ store.singleProfile.name }}"
+            />
           </div>
-          <div class="col-12 col-lg-6 d-flex flex-column justify-content-between ps-4">
+          <div
+            class="col-12 col-lg-6 d-flex flex-column justify-content-between ps-4"
+          >
             <div>
-              <h1 class="mb-3">{{ store.user.name }} {{ store.user.surname }}</h1>
+              <h1 class="mb-3">
+                {{ store.user.name }} {{ store.user.surname }}
+              </h1>
               <div class="specializations mb-3">
                 <h5 class="fs-4">Specializations:</h5>
                 <ul class="list-inline mb-0">
-                  <li v-for="specialization in store.singleProfile.specializations" class="list-inline-item me-2 fs-5">
+                  <li
+                    v-for="specialization in store.singleProfile
+                      .specializations"
+                    class="list-inline-item me-2 fs-5"
+                  >
                     <em>{{ specialization.name }}</em>
                   </li>
                 </ul>
               </div>
               <p class="mb-3 fs-5">
-                <strong>Phone number: </strong><br>
+                <strong>Phone number: </strong><br />
                 {{ store.singleProfile.phone_number }}
               </p>
               <p class="mb-3 fs-5">
-                <strong>Address: </strong><br>
+                <strong>Address: </strong><br />
                 {{ store.singleProfile.address }}
               </p>
               <p class="mb-3 fs-5">
-                <strong>Doctor email: </strong><br>
+                <strong>Doctor email: </strong><br />
                 {{ store.user.email }}
               </p>
               <p class="mb-3 fs-5">
-                <strong>About this doctor: </strong><br>
+                <strong>About this doctor: </strong><br />
                 {{ store.singleProfile.performances }}
               </p>
             </div>
 
             <div>
               <!-- download doctor curriculum -->
-              <a class="btn btn-primary mt-3" v-show="store.singleProfile.cv"
-                :href="store.url_image + store.singleProfile.cv" role="button">Doctor Curriculum</a>
+              <a
+                class="btn btn-primary mt-3"
+                v-show="store.singleProfile.cv"
+                :href="store.url_image + store.singleProfile.cv"
+                role="button"
+                >Doctor Curriculum</a
+              >
 
               <!-- Modal -->
               <div class="col mt-3">
-                <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#modalId">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-lg"
+                  data-bs-toggle="modal"
+                  data-bs-target="#modalId"
+                >
                   Send Message to Doctor
                 </button>
-                <div class="modal fade" id="modalId" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false"
-                  role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                <div
+                  class="modal fade"
+                  id="modalId"
+                  tabindex="-1"
+                  data-bs-backdrop="static"
+                  data-bs-keyboard="false"
+                  role="dialog"
+                  aria-labelledby="modalTitleId"
+                  aria-hidden="true"
+                >
                   <div
                     class="modal-dialog modal-xl modal-fullscreen-sm-down modal-dialog-scrollable modal-dialog-centered"
-                    role="document">
+                    role="document"
+                  >
                     <div class="modal-content">
                       <div class="modal-header">
                         <h5>Scrivi un messaggio al Dottore</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button
+                          type="button"
+                          class="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
                       </div>
                       <form action="" method="get">
                         <div class="modal-body">
                           <MessageDoctor></MessageDoctor>
                         </div>
                         <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                          <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                          >
                             Close
                           </button>
                         </div>
@@ -128,7 +167,6 @@ export default {
     </div>
   </div>
   <!-- profile info Doctor -->
-
   <div id="vote_doctor" class="container mt-4">
     <div class="row">
       <div class="col-sm-12 col-lg-6 bg-lighter-gray">
@@ -139,23 +177,46 @@ export default {
           <h4>Add vote to this doctor</h4>
           <form :action="store.api_url + 'api/votes/'" method="POST">
             <div class="mb-3">
-              <input type="hidden" :value="store.singleProfile.id" name="profile_id" />
+              <input
+                type="hidden"
+                :value="store.singleProfile.id"
+                name="profile_id"
+              />
               <input type="hidden" :value="'2023-07-07 15:30:00'" name="time" />
               <input type="hidden" name="vote" :value="vote" />
-              <input type="hidden" name="slug" :value="store.singleProfile.slug" />
-              <span v-for="i in 5" :key="i" class="rating-star" @click="openConfirmationModal(i)"
-                @mouseover="setHoverVote(i)" @mouseout="clearHoverVote">
+              <input
+                type="hidden"
+                name="slug"
+                :value="store.singleProfile.slug"
+              />
+              <span
+                v-for="i in 5"
+                :key="i"
+                class="rating-star"
+                @click="openConfirmationModal(i)"
+                @mouseover="setHoverVote(i)"
+                @mouseout="clearHoverVote"
+              >
                 <i :class="[hoverVote >= i ? 'fas' : 'far', 'fa-star']"></i>
               </span>
             </div>
 
             <!-- Modale di conferma voto -->
-            <div class="modal" ref="confirmationModal" @click.self="closeConfirmationModal">
+            <div
+              class="modal"
+              ref="confirmationModal"
+              @click.self="closeConfirmationModal"
+            >
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
                     <h5 class="modal-title">Conferma voto</h5>
-                    <button type="button" class="close" data-dismiss="modal" @click="closeConfirmationModal">
+                    <button
+                      type="button"
+                      class="close"
+                      data-dismiss="modal"
+                      @click="closeConfirmationModal"
+                    >
                       <span>&times;</span>
                     </button>
                   </div>
@@ -163,7 +224,12 @@ export default {
                     <p>Do you want to send this vote?</p>
                   </div>
                   <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="closeConfirmationModal">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-dismiss="modal"
+                      @click="closeConfirmationModal"
+                    >
                       Annulla
                     </button>
                     <button type="submit" class="btn btn-primary">Yes</button>
@@ -213,7 +279,6 @@ export default {
                 <div class="bar"></div>
                 <div class="percent">70%</div>
               </section>
-
             </article>
           </div>
         </div>
@@ -222,7 +287,6 @@ export default {
     </div>
   </div>
   <!-- vote container -->
-
   <div id="review_doctor" class="container bg-light-gray mt-4">
     <div class="row">
       <div class="col-sm-12 col-lg-6 bg-lighter-gray">
