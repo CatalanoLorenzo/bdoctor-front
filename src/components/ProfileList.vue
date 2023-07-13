4ac66bf4ff88163eeb45ecd944115f3e2c103baa
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-13 14:51:52 +0200

    add style to sponsored cards

--- src/components/ProfileList.vue
+++ src/components/ProfileList.vue
@@ -49,36 +49,44 @@ export default {
                     <i class="fa-sharp fa-solid fa-arrow-right fa-lg"></i>
                 </div>
             </div>
+
             <div class="row row-cols-1 row-cols-lg-3 position-relative">
                 <div v-for="(profile, index) in store.profiles.slice(currentSlide, currentSlide + 3).concat(store.profiles.slice(0, Math.max(0, 3 - store.profiles.length + currentSlide)))"
                     :key="index" class="col my-3">
-                    <div class="doctor_card card h-100 border-0 d-flex flex-column">
-                        <router-link :to="{ name: 'single-profile', params: { slug: profile.slug } }"
-                            class="text-decoration-none text-dark ">
-                            <div class="d-flex flex-column justify-content-between">
-                                <img class="card-img-top ratio-1x1 object-fit-cover" height="300" width="300"
-                                    :src="store.url_image + profile.doctor_image" :alt="profile.slug">
-                                <div class="card-body">
+                    <router-link :to="{ name: 'single-profile', params: { slug: profile.slug } }"
+                        class="text-decoration-none text-dark ">
+
+                        <div class="doctor_card card h-100 border-0 d-flex flex-column justify-content-between">
+
+                            <div class="top_card d-flex">
+                                <div class="image_single_doctor m-3 ">
+                                    <img class="rounded-circle" :src="store.url_image + profile.doctor_image"
+                                        :alt="profile.slug">
+                                </div>
+                                <div class=" card-body col-6">
                                     <h4 class="card-title ">{{ profile.user.name + ' ' +
                                         profile.user.surname }}</h4>
                                     <small>{{ profile.user.address }}</small><br>
                                     <small>{{ profile.user.email }}</small>
-                                    <p class="card-text">{{ profile.performances }}</p>
-                                </div>
-                                <div class="phone_number_section card-footer d-flex justify-content-center">
-                                    <b class="me-2 text-light">Phone:</b>
-                                    <h6 class="text-white mb-0 align-self-center">{{ profile.phone_number }}</h6>
+                                    <p class="card-text text_section_card">{{ profile.performances }}</p>
                                 </div>
                             </div>
-                        </router-link>
-                    </div>
-                    <!-- /card -->
+                            <div class="phone_number_section card-footer d-flex justify-content-center">
+                                <b class="me-2 text-light">Phone:</b>
+                                <h6 class="text-white mb-0 align-self-center">{{ profile.phone_number }}</h6>
+                            </div>
+
+
+                        </div>
+                    </router-link>
+
                 </div>
-
-
-
+                <!-- /card -->
             </div>
+
+
+
         </div>
-        <!-- /profiles -->
     </div>
+    <!-- /profiles -->
 </template>
