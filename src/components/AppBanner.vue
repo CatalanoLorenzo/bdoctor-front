50709948adc736f2be4deb3c5a30fc9fd8bc7aea
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-13 14:29:29 +0200

    add carousel

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
-  name:AppBanner,
-    data() {
+  name: "AppBanner",
+  data() {
     return {};
   },
 };
--- src/components/ProfileList.vue
+++ src/components/ProfileList.vue
@@ -5,26 +5,53 @@ export default {
     data() {
         return {
             store,
+            currentSlide: 0
+        }
+    },
+    methods: {
+        nextSlide() {
+            if (this.currentSlide === this.store.profiles.length - 1) {
+                this.currentSlide = 0;
+            } else {
+                this.currentSlide++;
+            }
+        },
+        prevSlide() {
+            if (this.currentSlide === 0) {
+                this.currentSlide = this.store.profiles.length - 1;
+            } else {
+                this.currentSlide--;
+            }
         }
     },
     mounted() {
-
-        // call api profiles mounted
-        const url_profiles = store.api_url + store.api_profile
+        const url_profiles = store.api_url + store.api_profile;
         this.store.getProfiles(url_profiles);
 
-        // call api specializations mounted
-        const url_specializations = store.api_url + store.api_specializations
+        const url_specializations = store.api_url + store.api_specializations;
         this.store.getSpecializations(url_specializations);
+
+        setInterval(this.nextSlide, 2000);
     }
 }
 </script>
+
 <template>
     <div>
-        <div id="card_doctor" class="container">
+        <div id="card_doctor" class="container position-relative">
             <h1>Sponsorized Doctors</h1>
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                <div v-for="profile in store.profiles" class="col my-3">
+            <!-- buttons -->
+            <div class="buttons d-flex w-100 justify-content-center gap-3">
+                <div @click="prevSlide" class="bg_opacity p-4 rounded-circle text-white">
+                    <i class="fa-sharp fa-solid fa-arrow-left fa-lg"></i>
+                </div>
+                <div @click="nextSlide" class="bg_opacity p-4 rounded-circle text-white">
+                    <i class="fa-sharp fa-solid fa-arrow-right fa-lg"></i>
+                </div>
+            </div>
+            <div class="row row-cols-1 row-cols-lg-3 position-relative">
+                <div v-for="(profile, index) in store.profiles.slice(currentSlide, currentSlide + 3).concat(store.profiles.slice(0, Math.max(0, 3 - store.profiles.length + currentSlide)))"
+                    :key="index" class="col my-3">
                     <div class="doctor_card card h-100 border-0 d-flex flex-column">
                         <router-link :to="{ name: 'single-profile', params: { slug: profile.slug } }"
                             class="text-decoration-none text-dark ">
@@ -47,6 +74,9 @@ export default {
                     </div>
                     <!-- /card -->
                 </div>
+
+
+
             </div>
         </div>
         <!-- /profiles -->
