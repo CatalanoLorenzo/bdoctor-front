e285019df29ade9b3f2e96ab8473c234e630c978
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-07-13 13:01:05 +0200

    create new partials scss for style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,22 @@
 <script>
-import ProfileList from '../components/ProfileList.vue';
-import SingleProfile from './SingleProfile.vue';
-import JumboTron from '../components/JumboTron.vue';
+import ProfileList from "../components/ProfileList.vue";
+import SingleProfile from "./SingleProfile.vue";
+import JumboTron from "../components/JumboTron.vue";
+import AppBanner from "../components/AppBanner.vue";
 
 export default {
-    name: "HomeView",
-    components: {
-        ProfileList,
-        SingleProfile,
-        JumboTron
-        }
-
-}
+  name: "HomeView",
+  components: {
+    ProfileList,
+    SingleProfile,
+    JumboTron,
+    AppBanner,
+  },
+};
 </script>
-        
+
 <template>
-    <JumboTron></JumboTron>
-    <ProfileList></ProfileList>
+  <JumboTron></JumboTron>
+  <ProfileList></ProfileList>
+  <AppBanner></AppBanner>
 </template>
