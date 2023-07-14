d64ec47614fa01396808f6d015773ad987013385
MarioBlasi <mblasi509@gmail.com>
2023-07-14 07:53:27 +0100

    toggleMenu header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,70 +1,59 @@
 <script>
 export default {
-  name: "AppHeader"
+  name: "AppHeader",
+  data() {
+    return {
+      isMenuOpen: false
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    }
+  }
 };
 </script>
 
 <template>
-  <div class="flex-container header fixed-top">
-    <div class="row">
-      <nav class="navbar navbar-expand-md navbar-light bg-white">
-        <div
-          class="container d-flex justify-content-center align-items-center p-4"
-        >
-          <div class="col-4">
-            <a class="navbar-brand d-flex" href="/">
-              <div class="logo_laravel navbar-expand-sm px-2 px-sm-0">
-                <div class="fadeIn">
-                  <img
-                    class="w-25"
-                    src="../assets/img/bdoctor__2_-removebg-previewPEPPE.png"
-                    id="icon"
-                    alt="User Icon"
-                  />
-                </div>
-              </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light custom-navbar">
+    <div class="container">
+      <img
+        class="w-25"
+        src="../assets/img/bdoctor__2_-removebg-previewPEPPE.png"
+        id="icon"
+        alt="User Icon"
+      />
+      <button
+        class="navbar-toggler"
+        type="button"
+        @click="toggleMenu"
+        :aria-expanded="isMenuOpen"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" :class="{ show: isMenuOpen }">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:8000/login">
+              <button class="btn btn-primary">Login</button>
             </a>
-          </div>
-          <!-- Left Side Of Navbar -->
-          <div class="col-2">
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div
-              class="collapse navbar-collapse position-fixed"
-              id="navbarSupportedContent"
-            >
-              <!-- Right Side Of Navbar -->
-              <ul class="navbar-nav align-content-end">
-                <!-- Authentication Links -->
-                <a
-                  class="btn btn-outline-primary btn-sm m-1"
-                  href="http://127.0.0.1:8000/login"
-                  role="button"
-                  >Login</a
-                >
-                <a
-                  class="btn btn-outline-primary btn-sm m-1"
-                  href="http://127.0.0.1:8000/register"
-                  role="button"
-                  >Register</a
-                >
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:8000/register">
+              <button class="btn btn-primary">Register</button>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-
-  <div class="elemento-successivo"></div>
+  </nav>
 </template>
+<style>
+.container img {
+  transform: scale(2.5);
+}
+
+/* .container {
+  height: 120px;
+} */
+</style>
