21c47770f3e4af8865fc99aedabc3fd35910a559
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-07-16 16:20:06 +0200

    add response card

--- src/components/ProfilesFiltered.vue
+++ src/components/ProfilesFiltered.vue
@@ -12,8 +12,34 @@ export default {
 };
 </script>
 <template>
-  <!-- card -->
   <div class="container">
+    <div class="row row-cols-1 row-cols-md-2 g-2">
+      <div class="col  p-3" v-for="profile in store.profiles">
+        <div class="box shadow-lg p-3 h-100">
+          <router-link class="text-decoration-none" :to="{ name: 'single-profile', params: { slug: profile.slug } }">
+            <div class="row row-cols-2 row-cols-md-3 flex-nowrap">
+              <div class="col p-0 img-container object-fit-cover  overflow-hidden d-flex justify-content-center">
+                <img class="" :src="store.url_image + profile.doctor_image" :alt="profile.slug" />
+              </div>
+              <div class="col data-doc text-black">
+                <p class="mb-0">{{ profile.user.name + " " + profile.user.surname }} <br></p>
+                <p class="mb-0">Phone: <br> {{ profile.phone_number }}</p>
+              </div>
+              <div class="col info-box text-black d-flex flex-column flex-md-row ">
+                <p class="mb-0 ">Address: {{ profile.user.address }} E-mail: {{ profile.user.email }}</p>
+                <!-- <p class="mb-0 w-50">E-mail: {{ profile.user.email }}</p> -->
+                <p class="mb-0">Performances:<p class="performances-show overflow-y-scroll mb-0">{{ profile.performances }}</p></p>
+              </div>
+            </div>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- card -->
+  <!--  <div class="container">
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
       <div v-for="profile in store.profiles" class="col my-3">
         <div class="doctor_card card h-100">
@@ -34,8 +60,8 @@ export default {
             </div>
           </router-link>
         </div>
-        <!-- /card -->
-      </div>
-    </div>
+         /card 
   </div>
+  </div>
+  </div> -->
 </template>
