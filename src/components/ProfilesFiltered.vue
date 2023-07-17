11e075e28e59647b3a30b576d4b7c12179a1f9df
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-07-17 09:26:38 +0200

    add my for col

--- src/components/ProfilesFiltered.vue
+++ src/components/ProfilesFiltered.vue
@@ -14,11 +14,11 @@ export default {
 <template>
   <div class="container">
     <div class="row ">
-      <div class="col-12 col-md-6 box-doctor rounded-5" v-for="profile in store.profiles " >
+      <div class="col-12 col-md-6 box-doctor rounded-5 my-2 " v-for="profile in store.profiles " >
         <router-link class="text-decoration-none h-100" :to="{ name: 'single-profile', params: { slug: profile.slug } }">
           <div class="row shadow-lg px-5 pt-3 w-100 h-100">
 
-            <div class="col-6 px-0 img-container object-fit-cover  overflow-hidden d-flex justify-content-center rounded-circle">
+            <div class="col-6 px-0 img-container object-fit-container  overflow-hidden d-flex justify-content-center rounded-circle">
               <img class="img-fluid" :src="store.url_image + profile.doctor_image" :alt="profile.slug" />
             </div>
             <div class="col-6  ">
